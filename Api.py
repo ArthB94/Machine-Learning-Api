62a490a0688e0be4935cfa8e960c351b479a3553
Hugo PANEL <hugopanel@gmail.com>
2024-06-18 00:28:37 +0200

    Working API with /predict endpoint

--- Api.py
+++ Api.py
@@ -1,25 +1,36 @@
-
 from fastapi import FastAPI
 import pickle
 import numpy as np
-from typing import List
+from typing import List, Union
+
+from pydantic import BaseModel
 
 app = FastAPI()
 
 # Charger le modèle
-with open("model.pkl", "rb") as f: # Ouvrir le fichier en mode lecture binaire
+with open("model.pkl", "rb") as f:  # Ouvrir le fichier en mode lecture binaire
     model = pickle.load(f)
+    print("Model loaded")
 
 
+class Features(BaseModel):
+    features: List[Union[
+        int, bool, bool, bool, int, float, float, float, float, float, float, float, float, float, float, float, float,
+        float, float, float, float, float, float, float, float, float, float, float, float, float, float, float, float,
+        float, float, float, float]]
 
-@app.post("/predict") 
-def predict(features: List[float]): # Prend une liste de float en entrée
+
+@app.post("/predict")
+def predict(features: Features):  # Prend une liste en entrée
+    print("Predict endpoint hit with features: %s", features.features)
     # Convertir les features en numpy array
-    input_features = np.array(features).reshape(1, -1)
+    input_features = np.array(features.features).reshape(1, -1)
     # Faire la prédiction
     prediction = model.predict(input_features)
     return {"prediction": prediction.tolist()}
 
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
